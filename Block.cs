eeb32530625f3f31e39e96b9237b3689d2e873e8
Helen Nguyen <hnguye89@uncc.edu>
2023-01-10 00:17:05 -0500

    Add subclasses, constructor, and method with documentation

--- Block.cs
+++ Block.cs
@@ -1,7 +1,32 @@
-﻿namespace Tetris
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Tetris
 {
     public abstract class Block
     {
-
+        /* A 2D position array which containts the tile positions in the four rotation states */
+        protected abstract Position[][] Tiles { get; }
+        /* A start offset which decides where the block spawns in the grid */
+        protected abstract Position StartOffset { get; }
+        /* An integer ID which distinguish the blocks */
+        public abstract int Id { get; }
+        /* store the current rotation state and the curreny offset */
+        private int rotationState;
+        private Position offset; 
+        /* contsructor */
+        public Block()
+        {
+            offset = new Position(StartOffset.Row, StartOffset.Column);
+        }
+        /* A method which returns the grid positions occupied by the block factoring in the current rotation 
+         * and offset */
+        public IEnumerable<Position> TilePositions()
+        {
+            foreach (Position p in Tiles[rotationState])
+            {
+                yield return new Position(p.Row + offset.Row, p.Column + offset.Column);
+            }
+        }
     }
 }
