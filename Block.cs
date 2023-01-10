f573a1811588592be07417196f035a3ba07629f7
Helen Nguyen <hnguye89@uncc.edu>
2023-01-09 23:57:02 -0500

    Change the public abstract class in Block

--- Block.cs
+++ Block.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tetris
+﻿namespace Tetris
 {
-    internal class Block
+    public abstract class Block
     {
+
     }
 }
