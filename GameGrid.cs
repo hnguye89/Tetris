18b2c90bac5dbed92b517553f860927b4e42f267
Helen Nguyen <hnguye89@uncc.edu>
2023-01-09 19:04:14 -0500

    Add project files.

---
+++ GameGrid.cs
@@ -0,0 +1,19 @@
+﻿namespace Tetris
+{
+    public class GameGrid
+    {
+        private readonly int[,] grid; 
+        public int Rows { get; }
+        public int Columns { get; }
+        public int this[int r, int c]
+        {
+            get => grid[r, c];
+            set => grid[r, c] = value;
+        }
+
+        public GameGrid(int rows,)
+        {
+
+        }
+    }
+}
---
+++ Tetris.cs
@@ -0,0 +1,3 @@
+﻿Public Class Tetris
+
+End Class
