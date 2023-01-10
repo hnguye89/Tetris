b878238cd1e83217ac0f1d2c8abf644d41a904fa
Helen Nguyen <hnguye89@uncc.edu>
2023-01-09 23:41:29 -0500

    Add constructor Position and store row and column

--- Position.cs
+++ Position.cs
@@ -2,5 +2,14 @@
 {
     internal class Position
     {
+        /* store row and column */
+        public int Row { get; set; }
+        public int Column { get; set; }
+        /*  constructor */ 
+        public Position(int row, int column)
+        {
+            Row= row;
+            Column= column;
+        }
     }
 }
