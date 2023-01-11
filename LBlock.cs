337dd01eadd5db3a09824668ac37d9670a684878
Helen Nguyen <hnguye89@uncc.edu>
2023-01-11 09:44:51 -0500

    Add jBlock class and  make some changes to other class

--- LBlock.cs
+++ LBlock.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tetris
+﻿namespace Tetris
 {
-    internal class LBlock
+    /* LBlock inherits Block */
+    public class LBlock : Block
     {
     }
 }
--- jBlock.cs
+++ jBlock.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tetris
+﻿namespace Tetris
 {
-    internal class jBlock
+    /* jBlock inherits Block class */
+    public class jBlock : Block
     {
+        public override int Id => 2;
+        protected override Position StartOffset => new Position(0, 3);
+        protected override Position[][] Tiles => new Position[][]
+        {
+            new Position[] {new(0,0), new(1,0), new(1,1), new(1,2)},
+            new Position[] {new(0,1), new(0,2), new(0,2), new(2,1)},
+            new Position[] {new(1,0), new(1,1), new(1,2), new(2,2)},
+            new Position[] {new(0,1), new(1,1), new(2,1), new(2,0)}
+        };
     }
 }
