c033e7303d03293b241d37215a6360d487ee23ab
Helen Nguyen <hnguye89@uncc.edu>
2023-01-10 22:58:46 -0500

    Add modifiers and documentation

--- iBlock.cs
+++ iBlock.cs
@@ -10,7 +10,13 @@
             new Position[] {new(2,0), new(2,1), new(2,2), new(2,3)},
             new Position[] {new(0,1), new(1,1), new(2,1), new(3,1)}
         };
-
+        /* Filled out the required properties */
+        public override int Id => 1;
+        /* The start offset should be -1. */
+        /* This will make the block spawn in the middle of the top row */
+        protected override Position StartOffset => new Position(-1,3);
+        /* for the tiles property, this return the tiles array above */
+        protected override Position[][] Tiles => tiles;
 
     }
 }
