3f990bc88ed1202b07deadb652b62dd89c90adf8
Helen Nguyen <hnguye89@uncc.edu>
2023-01-10 22:36:48 -0500

    Add method Position and documentation

--- iBlock.cs
+++ iBlock.cs
@@ -1,12 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tetris
+﻿namespace Tetris
 {
-    internal class iBlock
+    public class iBlock : Block
     {
+        /* This store the tile positions for the four rotation states */
+        private readonly Position[][] tiles = new Position[][]
+        {
+            new Position[] {new(1,0), new(1,1), new(1,2), new(1,3)},
+            new Position[] {new(0,2), new(1,2), new(2,2), new(3,2)},
+            new Position[] {new(2,0), new(2,1), new(2,2), new(2,3)},
+            new Position[] {new(0,1), new(1,1), new(2,1), new(3,1)}
+        };
+
+
     }
 }
