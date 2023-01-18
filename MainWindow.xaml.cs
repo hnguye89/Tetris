8203c5af958357b8f27d5d2b90eb3817c67f3485
Helen Nguyen <hnguye89@uncc.edu>
2023-01-18 17:51:26 -0500

    Add coddes in MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,6 +20,31 @@ namespace Tetris
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// Array full of tile images
+        /// </summary>
+        private readonly ImageSource[] tileImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("assets/TileEmpty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TileCyan.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TileBlue.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TileOrange.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TileYellow.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TileGreen.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TilePurple.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/TileRed.png", UriKind.Relative))
+        };
+        private readonly ImageSource[] blockImages = new ImageSource[]
+        {
+            new BitmapImage(new Uri("assets/Block-Empty.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-I.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-J.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-L.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-O.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-S.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-T.png", UriKind.Relative)),
+            new BitmapImage(new Uri("assets/Block-Z.png", UriKind.Relative))
+        };
         public MainWindow()
         {
             InitializeComponent();
